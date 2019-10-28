3f041aec0cdfb592e3a194f9e8872ca9dd2203c2
AjeebAadmi <57108152+AjeebAadmi@users.noreply.github.com>
2019-10-28 22:37:01 +0530

    Add files via upload

---
+++ HACKERMA.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+	{
+	float a,b;
+	float sum=0,pro=1,div=1,avg=1;
+	clrscr();
+	printf("entertwo numbers\n");
+	scanf("%f%f",&a,&b);
+	sum=a+b;
+	printf("sum=%f",sum);
+	pro=a*b;
+	printf("\nproduct=%f",pro);
+	div=a/b;
+	printf("\nquotient=%f",div);
+	avg=(a+b)/2;
+	printf("\naverage=%f",avg);
+	getch();
+
+
+
+
+
+
+
+
+}
