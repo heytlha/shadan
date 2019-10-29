c3efebf7a512ed509812b1067d9a6d85d1d26eb4
AjeebAadmi <57108152+AjeebAadmi@users.noreply.github.com>
2019-10-30 02:02:25 +0530

    Add files via upload

---
+++ 22222.c.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{     float a;
+clrscr();
+printf("enter any number\n");
+scanf("%f",&a);
+if(a==1)
+printf("dog");
+else
+printf("cat");
+getch();
+}
+
+
+
+
+
+
+
